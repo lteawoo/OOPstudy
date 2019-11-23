0fb37a4eeff00474379efcddc39bb4819c56b434
Taewoo Lee <lteawoo@naver.com>
2019-11-23 16:58:17 +0900

    스프링을 통한 의존성 주입-> test파일 작성중

---
+++ Expert003/src/test/java/expert003/CarTest.java
@@ -0,0 +1,11 @@
+package expert003;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("expert003.xml")
+public class CarTest {
+
+}
