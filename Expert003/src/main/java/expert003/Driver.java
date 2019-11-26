f79ac92baed0a1cd696a62f5bdc61aa170b3cbf6
Taewoo Lee <lteawoo@naver.com>
2019-11-26 20:16:10 +0900

    @Resources를 통한 속성 주입 java 표준 어노테이션이다. Autowired가 type이 우선순위가 높았으면 Resources는 id가 우선순위가 높다.

--- Expert003/src/main/java/expert003/Driver.java
+++ Expert003/src/main/java/expert003/Driver.java
@@ -2,10 +2,11 @@ package expert003;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+/* classpath: main 아래부터.. */
 public class Driver {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("expert003.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("classpath:expert003/expert003.xml");
 
         Car car = context.getBean("car", Car.class);
 
